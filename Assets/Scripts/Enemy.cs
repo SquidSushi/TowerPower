02b0ec4efe7e45c72194cf6a12bebd9c5b5606f1
Oliver Pruchnicki <35111378+SquidSushi@users.noreply.github.com>
2026-05-12 12:38:40 +0200

    Some Adjustments

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,9 +1,11 @@
+using System;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour{
     public EnemyPathNode Target;
 
     public float Speed;
+    public float TurnSpeed = 1080;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,6 +19,16 @@ public class Enemy : MonoBehaviour{
     void Update()
     {
         if (Target == null) return;
+        Vector3 directionToTarget = Target.transform.position - transform.position;
+        directionToTarget.Normalize();
+        float rightDot = Vector3.Dot(transform.right,directionToTarget);
+        if (Mathf.Abs(rightDot)<0.05f){
+            transform.LookAt(Target.transform.position);
+        }
+        else{
+            float sign = rightDot > 0 ? 1 : -1;
+            transform.Rotate(0,sign * TurnSpeed * Time.deltaTime,0);
+        }
         transform.Translate(Vector3.forward * (Speed * Time.deltaTime)); //Vorwärts bewegen
         if (Vector3.Distance(transform.position, Target.transform.position) <= 0.1f){
             // Wenn wir grob auf dem Ziel sind:
@@ -26,7 +38,7 @@ public class Enemy : MonoBehaviour{
             }
             else{
                 Target = Target.Next; // Ändere das Ziel auf das nächste
-                transform.LookAt(Target.transform.position); // Drehe zum nächsten Ziel
+                //transform.LookAt(Target.transform.position); // Drehe zum nächsten Ziel
             }
         }
     }
--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
 
-public class EnemySpawner : MonoBehaviour
-{
+public class EnemySpawner : MonoBehaviour{
+    public GameObject EnemyPrefab; //TODO Mehrere Prefabs
+    // Todo Waves
+    private float _nextSpawnTime;
+    public EnemyPathNode FirstNode;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    void Start(){
+        _nextSpawnTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_nextSpawnTime <= Time.time){ // Wenn der nächste Spawn-Zeitpunkt vergangen ist:
+            _nextSpawnTime = Time.time + 0.5f;
+            var newSpawn = Instantiate(EnemyPrefab, transform.position, Quaternion.identity);
+            newSpawn.GetComponent<Enemy>().Target = FirstNode;
+        }
     }
 }
