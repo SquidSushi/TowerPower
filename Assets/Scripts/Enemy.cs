4224186d82ef2c6221f2718dca5c0b900c17659c
Oliver Pruchnicki <35111378+SquidSushi@users.noreply.github.com>
2026-05-12 15:02:10 +0200

    Deutsche sprache

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -41,7 +41,7 @@ public class Enemy : MonoBehaviour{
                 //transform.LookAt(Target.transform.position); // Drehe zum nächsten Ziel
             }
         }
-        // Check ob wir am Ziel vorbei sind UND nah dran:
+        // Prüfen ob wir am Ziel vorbei sind UND nah dran:
         float fwDot = Vector3.Dot(transform.forward, directionToTarget);
         if (Vector3.Distance(transform.position, Target.transform.position) < 1 && fwDot < 0){
             
